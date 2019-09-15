9b3aa236aef3e665c3b05368788e85006e45a48c
CLIFF CRELIN <cliff.crelin@gmail.com>
2019-09-15 20:09:56 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
    i = 0
   collection = []
   while i < languages.length
-    collection << yield(language [i])
+    collection << yield(languages [i])
     i += 1
   end
   collection
